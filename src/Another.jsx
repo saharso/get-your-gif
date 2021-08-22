9e3c7ffe6336f83b273991b22d04f920fcfb277c
Sahar Soffer <ssoffer@medallia.com>
2021-08-22 07:25:21 +0300

    delete useless file

--- src/Another.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import _ from 'lodash';
-export default function Another(){
-    return <div>
-        another location
-        <br/>
-        {_.join(['foo', 'fighter'], ' ')}
-    </div>
-}
